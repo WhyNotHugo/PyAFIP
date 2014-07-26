fb302dd73accdadb906cd7659cc031a2a3e36cd0
Hugo Osvaldo Barrera <hugo@barrera.io>
2014-07-26 09:51:40 -0300

    Tiny bit of documentation.

--- test/wsaa.py
+++ test/wsaa.py
@@ -3,6 +3,7 @@
 from afip.endpoints import Testing
 from afip.wsaa import AuthorizationTicket
 
+# The key and certificate are the only values you should need to change.
 ticket = AuthorizationTicket(Testing(), "hyperion.key", "hyperion.crt", "wsfe")
 ticket.login()
 
